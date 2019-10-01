f41cc6b4f56c1e40b859f84ed6cc93134194675f
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-01 12:52:12 +0900

    my 3 commit

--- calc.py
+++ calc.py
@@ -1,10 +1,25 @@
 
 def calc(s):
     print("s=",s)
-    nums=map(int,s.split("+"))
-    #print("nums=",nums)
+    nums=s.split("+")
+    m=len(nums)
+    for i in range(m):
+      a=1
+      if "*" in nums[i]:
+        num=nums[i].split("*")
+        n=len(num)
+        for j in range(n):
+          num[j]=int(num[j])
+          a=a*num[j]
+          if j==n-1:
+            nums[i]=int(a)
+            
+      else:
+        nums[i]=int(nums[i])
+          
     return sum(nums)
 
 print(calc("1"))
 print(calc("1+2"))
-print(calc("1+2+3"))
+print(calc("1*2+3"))
+print(calc("1+2*3"))
