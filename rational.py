d781b283c8371b29d95b6ea939493ae6350e0d20
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-07 16:10:57 +0900

    added gcd commit

--- rational.py
+++ rational.py
@@ -1,22 +1,10 @@
 class Q(object):
-    def __init__(self):#コンストラクタ
-        self.a=0
-        self.b=0
-    def reset(self):
-        self.a=0
-        self.b=0
+    def __init__(self,a,b):#コンストラクタ
+        self.a=a
+        self.b=b
 
-    def show(self):
-        print(self.yuuri)
+    def __repr__(self):
+        return f'{self.a}/{self.b}'
 
-    def 
-
-    def __repr__(self): #文字列を返すと表示される
-        return str(self.cnt)
-
-c=Counter()
-c.show()
-c.doublecount()
-c.show()
-print(type(c))
-print(c)
+q=Q(1,2)
+print(q)
