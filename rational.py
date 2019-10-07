5df081227c201d2a8a7c9249ac03ada7e571463c
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-07 16:30:58 +0900

    all operators commit

--- rational.py
+++ rational.py
@@ -9,16 +9,41 @@ class Q(object):
         else:    
             return f'{self.a}/{self.b}'
 
-    def add(self,q):
+    def __add__(self,q):
         a=self.a
         b=self.b
         c=q.a
         d=q.b
         return Q(a*d+b*c,b*d)
 
-q=Q(1,2)
-q=Q(2,1)
+    def __sub__(self,q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d-b*c,b*d)
+
+    def __mul__(self,q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*c,b*d)
+
+    def __truediv__(self,q):
+        a=self.a
+        b=self.b
+        c=q.a
+        d=q.b
+        return Q(a*d,b*c)  
+
+    
+
 q1=Q(1,2)
 q2=Q(1,3)
-print(q1.add(q2))
-print(q)
+
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
+
