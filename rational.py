27823eb7acb184bf9fcc51000243e13a8933e0eb
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-07 15:53:56 +0900

    second commit

---
+++ rational.py
@@ -0,0 +1,21 @@
+class Yuuri(object):
+    def __init__(self):#コンストラクタ
+        self.yuuri=0
+
+    def 
+
+    def reset(self):
+        self.yuuri=0
+
+    def show(self):
+        print(self.yuuri)
+
+    def __repr__(self): #文字列を返すと表示される
+        return str(self.cnt)
+
+c=Counter()
+c.show()
+c.doublecount()
+c.show()
+print(type(c))
+print(c)
