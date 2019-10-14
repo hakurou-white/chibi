819f170567575eb657027c8b745b9057fc7f3d21
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-14 16:09:52 +0900

    sub commit

--- exp.py
+++ exp.py
@@ -37,3 +37,13 @@ e=Mul(Val(1),Val(2))
 assert e.eval()==2
 e=Mul(Val(1),Mul(Val(2),Val(3)))
 assert e.eval()==6        
+
+class Sub(object):
+    __slots__=['left','right']
+    def __init__(self,a,b):
+        self.left=a
+        self.right=b
+    def eval(self):
+        return self.left.eval()-self.right.eval()
+e=Sub(Val(1),Val(2))
+assert e.eval()==-1
