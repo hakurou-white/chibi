50e350e8db84310eef6d02ea90a08d1e5953e3d9
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-14 15:46:07 +0900

    val commit

--- exp.py
+++ exp.py
@@ -1,9 +1,14 @@
 class Val(object):
     __slots__= ['value']
-    def __init__(self,v=0):
-        self.value=v
+    def __init__(self,value=0):
+        self.value=value
+
+    def __repr__(self):
+        return f'Val({self.value})'
+        
     def eval(self):
         return self.value
 
-e=Val(1)
-assert e.eval()==1
+v=Val(1)
+print(v)
+assert v.eval()==1
