fa118b7b9dd27a0ce11295157e5cffc181addebb
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-28 16:35:01 +0900

    output add,mul

--- parse.py
+++ parse.py
@@ -1,13 +1,19 @@
-from exp import Val,Add
+from exp import Val,Add,Sub,Mul,Div
+
 def parse(s:str):
-    pos=s.find('+')
-    if pos==-1:
-        num=int(s)
-        return Val(num)
-    else:
-        s1=s[0:pos]
+    if s.find('+')>0:
+        pos=s.find('+')
+        s1=s[:pos]
         s2=s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('*')>0:
+        pos=s.find('*')
+        s1=s[:pos]
+        s2=s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    return Val(int(s))
 
-e=parse("123+456+789")
-print(e)
+e=parse("1*2+3")
+print(e,e.eval())
+e=parse("1+2*3")
+print(e,e.eval())
