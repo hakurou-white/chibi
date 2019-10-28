9e370878af554f3f7b77ab85fe4b98c1e78cfac9
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-28 16:15:25 +0900

    output binary add

--- parse.py
+++ parse.py
@@ -1,15 +1,13 @@
 from exp import Val,Add
 def parse(s:str):
-    num=int(s)
-    return Val(num)
+    pos=s.find('+')
+    if pos==-1:
+        num=int(s)
+        return Val(num)
+    else:
+        s1=s[0:pos]
+        s2=s[pos+1:]
+        return Add(Val(int(s1)),Val(int(s2)))
 
-e=parse("123")
+e=parse("123+456")
 print(e)
-
-s="1+2"
-pos=s.find('+')
-print('pos',pos)
-
-s1=s[0:pos]
-s2=s[pos+1:]
-print(s1,s2)    # +記号で分割
