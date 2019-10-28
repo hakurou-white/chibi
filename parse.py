55011030d41776fa2d4047f2cd1e4a24d9fd667d
SHIMIZU Runa <21716050@jwu.ac.jp>
2019-10-28 16:50:44 +0900

    output

--- parse.py
+++ parse.py
@@ -2,18 +2,35 @@ from exp import Val,Add,Sub,Mul,Div
 
 def parse(s:str):
     if s.find('+')>0:
-        pos=s.find('+')
+        pos=s.rfind('+')
         s1=s[:pos]
         s2=s[pos+1:]
         return Add(parse(s1),parse(s2))
+    if s.find('-')>0:
+        pos=s.rfind('-')
+        s1=s[:pos]
+        s2=s[pos+1:]
+        return Sub(parse(s1),parse(s2))
     if s.find('*')>0:
-        pos=s.find('*')
+        pos=s.rfind('*')
         s1=s[:pos]
         s2=s[pos+1:]
         return Mul(parse(s1),parse(s2))
+    if s.find('/')>0:
+        pos=s.rfind('/')
+        s1=s[:pos]
+        s2=s[pos+1:]
+        return Div(parse(s1),parse(s2))
     return Val(int(s))
 
 e=parse("1*2+3")
 print(e,e.eval())
 e=parse("1+2*3")
+print(e,e.eval())
+
+e=parse("1-2-3")
+print(e,e.eval())
+assert e.eval()==-4
+
+e=parse("12/4/3")
 print(e,e.eval())
