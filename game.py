b9b0d89312fb553a458b205c9f351e1d79ab9505
hakurou-white <hakurou178@gmail.com>
2019-12-15 20:01:10 +0900

    ball1

--- game.py
+++ game.py
@@ -1,6 +1,16 @@
 from tkinter import *
 import time
 import random
+
+class Ball:
+    def __init__(self,canvas,color):
+        self.canvas=canvas
+        self.id=canvas.create_oval(10,10,25,25,fill=color)
+        self.canvas.move(self.id,245,100)
+
+    def draw(self):
+        pass
+
 tk=Tk()
 tk.title("Game")
 tk.resizable(0,0)
@@ -8,3 +18,10 @@ tk.wm_attributes("-topmost",1)
 canvas=Canvas(tk,width=500,height=400,bd=0,highlightthickness=0)
 canvas.pack()
 tk.update()
+
+ball=Ball(canvas,'blue')
+
+while True:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
