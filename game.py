99071dd7fb96d59a26fa2f242f409c717b0c9272
hakurou-white <hakurou178@gmail.com>
2019-12-17 16:46:50 +0900

    paddle ateru

--- game.py
+++ game.py
@@ -3,8 +3,9 @@ import time
 import random
 
 class Ball:
-    def __init__(self,canvas,color):
+    def __init__(self,canvas,paddle,color):
         self.canvas=canvas
+        self.paddle=paddle
         self.id=canvas.create_oval(10,10,25,25,fill=color)
         self.canvas.move(self.id,245,100)
         starts=[-3,-2,-1,1,2,3]
@@ -14,6 +15,13 @@ class Ball:
         self.canvas_height=self.canvas.winfo_height()
         self.canvas_width=self.canvas.winfo_width()       
 
+    def hit_paddle(self,pos):
+        paddle_pos=self.canvas.coords(self.paddle.id)
+        if pos[2]>=paddle_pos[0] and pos[0]<=paddle_pos[2]:
+            if pos[3]>=paddle_pos[1] and pos[3]<=paddle_pos[3]:
+                return True
+        return False
+
     def draw(self):
         self.canvas.move(self.id,self.x,self.y)
         pos=self.canvas.coords(self.id)
@@ -21,6 +29,8 @@ class Ball:
             self.y=1
         if pos[3]>=self.canvas_height:
             self.y=-1
+        if self.hit_paddle(pos)==True:
+            self.y=-3
         if pos[0]<=0:
             self.x=3
         if pos[2]>=self.canvas_width:
@@ -59,8 +69,8 @@ canvas=Canvas(tk,width=500,height=400,bd=0,highlightthickness=0)
 canvas.pack()
 tk.update()
 
-ball=Ball(canvas,'green')
 paddle=Paddle(canvas,'black')
+ball=Ball(canvas,paddle,'green')
 
 while True:
     ball.draw()
